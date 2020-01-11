eb17490dbd92b165f821ae37168969dd6a99f57b
유채원 <ohwow5@sookmyung.ac.kr>
2020-01-11 17:59:03 +0900

    Add requests로 페이지 정보 가져오기

--- PROJECT/crawling_first.py
+++ PROJECT/crawling_first.py
@@ -1,2 +1,6 @@
-import request
-from bs4 import Beautifulsoup
+import requests
+from bs4 import BeautifulSoup
+
+url = 'http://www.cgv.co.kr/common/showtimes/iframeTheater.aspx?areacode=01&theatercode=0013&date=20200115'
+html = requests.get(url)
+print(html.text)
