aaacab5b50266f70afa84806a35c4dd8433ce600
유채원 <ohwow5@sookmyung.ac.kr>
2020-01-11 18:13:35 +0900

    add bs4로 상영시간표 영화 제목 크롤링

--- PROJECT/crawling_first.py
+++ PROJECT/crawling_first.py
@@ -3,4 +3,8 @@ from bs4 import BeautifulSoup
 
 url = 'http://www.cgv.co.kr/common/showtimes/iframeTheater.aspx?areacode=01&theatercode=0013&date=20200115'
 html = requests.get(url)
-print(html.text)
+
+soup = BeautifulSoup(html.text, 'html.parser')
+title_list = soup.select('div.info-movie')
+for i in title_list:
+    print(i.select_one('a > strong').text.strip())
