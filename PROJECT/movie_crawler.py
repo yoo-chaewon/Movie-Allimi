6b88780e9637a51b29d4136a2b12fb316bfc895e
유채원 <ohwow5@sookmyung.ac.kr>
2020-01-11 19:11:28 +0900

    add 알리미에 스케줄러 추가하기

--- PROJECT/movie_crawler.py
+++ PROJECT/movie_crawler.py
@@ -1,15 +1,23 @@
 import requests
 import telegram
 from bs4 import BeautifulSoup
+from apscheduler.schedulers.blocking import BlockingScheduler
+
 
 bot = telegram.Bot(token = '1069181474:AAH84nrPpIYiTjTWW1F0iviYs5Z1LdIdUgw')
 url = 'http://www.cgv.co.kr/common/showtimes/iframeTheater.aspx?areacode=01&theatercode=0013&date=20200115'
-html = requests.get(url)
-soup = BeautifulSoup(html.text, 'html.parser')
-imax = soup.select_one('span.imax')
-if(imax):
-    imax = imax.find_parent('div', class_='col-times')
-    title = imax.select_one('div.info-movie > a > strong').text.strip()
-    bot.sendMessage(chat_id=862740368, text=title + 'IMAX 예매가 열렸습니다.')
-else:
-    bot.sendMessage(chat_id=862740368, text='IMAX 예매가 아직 열리지 않았습니다.')
+
+def job_function():
+    html = requests.get(url)
+    soup = BeautifulSoup(html.text, 'html.parser')
+    imax = soup.select_one('span.imax')
+    if (imax):
+        imax = imax.find_parent('div', class_='col-times')
+        title = imax.select_one('div.info-movie > a > strong').text.strip()
+        bot.sendMessage(chat_id=862740368, text=title + 'IMAX 예매가 열렸습니다.')
+    else:
+        bot.sendMessage(chat_id=862740368, text='IMAX 예매가 아직 열리지 않았습니다.')
+
+sched = BlockingScheduler()
+sched.add_job(job_function, 'interval', seconds=30)
+sched.start()
