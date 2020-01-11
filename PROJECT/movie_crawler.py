7aa25029ad90808a8b104fd6a78b6c710b5b1795
유채원 <ohwow5@sookmyung.ac.kr>
2020-01-11 18:21:23 +0900

    Add IMAX영화 예매 오픈 여부 크롤링 하기

---
+++ PROJECT/movie_crawler.py
@@ -0,0 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'http://www.cgv.co.kr/common/showtimes/iframeTheater.aspx?areacode=01&theatercode=0013&date=20200115'
+html = requests.get(url)
+soup = BeautifulSoup(html.text, 'html.parser')
+imax = soup.select_one('span.imax')
+if(imax):
+    print ('IMAX 예매가 열렸습니다.')
+else:
+    print('IMAX 예매가 아직 열리지 않았습니다.')
